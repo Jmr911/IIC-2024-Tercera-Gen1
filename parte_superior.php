dc2b6b9b0f1b391bd8eee29bd79280ffa55f5631
Jossymare <jmr91_@hotmail.com>
2024-06-12 21:47:51 -0600

    Primera Clase

---
+++ parte_superior.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Proyecto de Tercera 1" content="Página de Prueba">
+    <title>
+        Tercera 1
+</title>
+<link rel="stylesheet" href="./styles/main.css">
+
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <hr="./index.php"><img src="./images/menu.png" alt="">
+        <
+
+        <img src="./images/menu.png" alt="">
+        <p class="texto-menu-izquierda">
+            La Casa de Bob
+    </div>
+</menu>  
