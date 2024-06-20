d75a3aa74841805bc2cf856078d8fca62f92581e
Jossymare <jmr91_@hotmail.com>
2024-06-19 21:43:30 -0600

    Final Hamburguer Icon

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php require_once "./parte_superior.php"?>
 
-<div class="titulo">
-<p>Estudiar una carrera universitaria es un privilegio de pocos, ¿Qué estás esperando?</p>
-</div> 
 
+<div class="titulo">
+<p>Estudiar una carrera universitaria es un privilegio de pocos, ¿qué estás esperando?</p>
+
+</div>
 <body>
-<div class="main-img">
-    <img src="./Images/Clases-en-linea-1.png" alt="">
+    <div class="main-img">
+        <img src="./images/Clases-en-linea-1.png" alt="">
+    
     </div>
 </body>
 
 
-<?php require_once "./parte_inferior.php"?>
+<?php require_once "./parte_Inferior.php"?>
--- parte_superior.php
+++ parte_superior.php
@@ -3,23 +3,33 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="Proyecto de Tercera 1" content="Página de Prueba">
-    <title>
+    <meta name="Proyecto de Tecera 1" content="Pagina de prueba">
+    <!-- <title>
         Tercera 1
-</title>
-<link rel="stylesheet" href="./styles/main.css">
+    </title> -->
+    <link rel="stylesheet" href="./styles/main.css">
 </head>
 
 <menu>
     <div class="menu-izquierda">
         <a href="./index.php">
-            <img src=".\Images/logo1.jpeg" alt="">
+            <img src="./images/logo1.jpeg" alt="">
         </a>
     </div>
-
-    </div class= "menu-derecha">
-    <a class="enlaces-menu-derecha" href= "https://www.google.com">Primer Ingreso</a>
-    <a class="enlaces-menu-derecha" href= "https://www.google.com">Aula En Linea</a>
+    <div class="menu-container">
+        <div class="menu-derecha">
+            <a class="enlaces-menu-derecha" href="https://google.com">Primer Ingreso</a>
+            <a class="enlaces-menu-derecha" href="https://google.com">Aula en Linea</a>
+        </div>
+        <div class="hamburger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
 
-</menu>  
+</menu>
+
+
+
+</html>
